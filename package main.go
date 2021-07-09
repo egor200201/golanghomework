b905800472c0bb737deb4f7106ed44b5710c0725
egor200201 <egor20020130@outlook.com>
2021-07-09 19:36:26 +0300

    in one line

--- package main.go
+++ package main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/kyokomi/emoji"
 )
 
 func main() {
-	fmt.Println("hello world")
-	emoji.Println(":face_with_monocle:")
+	emoji.Println("hello world:face_with_monocle:")
 }
