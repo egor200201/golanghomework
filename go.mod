c4099b31cc3d2dd171886fb6b4328627578a339b
egor200201 <73081726+egor200201@users.noreply.github.com>
2021-07-11 16:48:57 +0300

    fibonachi

---
+++ go.mod
@@ -0,0 +1,5 @@
+module knocker
+
+go 1.16
+
+require github.com/egor200201/golanghomework v0.0.0-20210711131829-11cee4824fc4
--- task2/main.go
+++ task2/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"knocker/task2/fibbfunction"
 )
 
 func main() {
 	fmt.Println("welcome")
-	defer fmt.Println("fibb numbers")
+	defer fmt.Println("fibb numbers printed")
 	fibbfunction.Printer(10)
 }
