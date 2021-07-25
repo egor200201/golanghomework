e9ba597889e99b9af217195ce8e2c2cab5a3e683
egor200201 <73081726+egor200201@users.noreply.github.com>
2021-07-25 14:35:31 +0300

    changed geometria go and added tests

--- task4/geometria.go
+++ task4/geometria.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
 
 type Shape interface {
 	String() string
-	Area() float64
-	Perimeter() float64
+	Area() (float64, error)
+	Perimeter() (float64, error)
 }
 
 func DescribeShape(s Shape) {
-	fmt.Println(s)
-	fmt.Printf("Area: %.2f\n", s.Area())
-	fmt.Printf("Perimeter: %.2f\n", s.Perimeter())
+	var (
+		v float64
+		e error
+	)
+	v, e = s.Area()
+	if e != nil {
+		fmt.Println(e)
+	} else {
+		fmt.Println(s)
+		fmt.Printf("Area: %.2f\n", v)
+		v, _ = s.Perimeter()
+		fmt.Printf("Perimeter: %.2f\n", v)
+	}
 }
 
 type Circle struct {
@@ -26,28 +37,41 @@ type Rectangle struct {
 }
 
 func (c Circle) String() string {
+
 	return fmt.Sprintf("Cirlce: radius %f", c.radius)
 }
 
-func (c Circle) Area() float64 {
-	return c.radius * c.radius * math.Pi
+func (c Circle) Area() (float64, error) {
+	if c.radius <= 0 {
+		return 0, errors.New("cant calculate the values for circle")
+	}
+	return c.radius * c.radius * math.Pi, nil
 }
 
-func (c Circle) Perimeter() float64 {
-	return 2 * math.Pi * c.radius
+func (c Circle) Perimeter() (float64, error) {
+	if c.radius <= 0 {
+		return 0, errors.New("cant calculate the values for circle")
+	}
+	return 2 * math.Pi * c.radius, nil
 }
 
 func (r Rectangle) String() string {
+
 	return fmt.Sprintf("Rectangle with height %f and width %f", r.height, r.width)
 }
 
-func (r Rectangle) Area() float64 {
-	return r.width * r.height
+func (r Rectangle) Area() (float64, error) {
+	if (r.height <= 0) || (r.width <= 0) {
+		return 0, errors.New("cant calculate the values for rectangle")
+	}
+	return r.width * r.height, nil
 }
 
-func (r Rectangle) Perimeter() float64 {
-
-	return 2 * (r.width + r.height)
+func (r Rectangle) Perimeter() (float64, error) {
+	if (r.height <= 0) || (r.width <= 0) {
+		return 0, errors.New("cant calculate the values for rectangle")
+	}
+	return 2 * (r.width + r.height), nil
 }
 
 func main() {
